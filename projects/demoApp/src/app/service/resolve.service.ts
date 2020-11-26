946132df2a485298eb2e1b18d0953612a7f45dcf
akash.influxiq <akash.influxiq@gmail.com>
2020-11-26 16:09:47 +0530

    Merge branch 'master' of https://github.com/influxiq1/traning_lib

--- projects/demoApp/src/app/service/resolve.service.ts
+++ projects/demoApp/src/app/service/resolve.service.ts
@@ -5,7 +5,7 @@ import { HttpService } from './http.service';
 import { CookieService } from 'ngx-cookie-service';
 
 export interface EndpointComponent {
-    endpoint: string;
+  endpoint: string;
 }
 
 @Injectable({
@@ -13,42 +13,50 @@ export interface EndpointComponent {
 })
 
 export class ResolveService implements Resolve<any> {
-public allCookiesData:any;
-public cookiesData:any;
-public userType:any;
-public userId:any;
-public user_details:any;  
+  public allCookiesData: any;
+  public cookiesData: any;
+  public userType: any;
+  public userId: any;
+  public user_details: any;
 
-  constructor(private _apiService: HttpService, private router: Router,public cookiesService:CookieService ) { 
+  constructor(private _apiService: HttpService, private router: Router, public cookiesService: CookieService) {
     // this.allCookiesData = cookiesService.getAll();
     //   this.cookiesData = JSON.parse(this.allCookiesData.user_details);
     //   this.userType=this.cookiesData.type;
 
     //   this.userId = this.cookiesData._id;
     //   this.userType=this.cookiesData.type;
-      // console.log("routeee",this.acti);
+    // console.log("routeee",this.acti);
 
-        // this.user_details=JSON.parse(this.cookiesService.get('user_details'));
-        // console.log(this.user_details,"....?")
+    // this.user_details=JSON.parse(this.cookiesService.get('user_details'));
+    // console.log(this.user_details,"....?")
 
-        // this.userType=this.user_details.user_type;
-        // this.userId=this.user_details._id;
-        this.userType = JSON.parse(this.cookiesService.get('type'));
-         this.userId = JSON.parse(this.cookiesService.get('userid'));
+    // this.userType=this.user_details.user_type;
+    // this.userId=this.user_details._id;
+    if (this.cookiesService.get('type') != null && this.cookiesService.get('type') != '') {
+      this.userType = JSON.parse(this.cookiesService.get('type'));
+
+    }
+
+    if (this.cookiesService.get('userid') != null && this.cookiesService.get('userid') != '') {
+      this.userId = JSON.parse(this.cookiesService.get('userid'));
+
+    }
+    // this.userId = JSON.parse(this.cookiesService.get('userid'));
 
   }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {     
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
     /* will come into play while editing otherwise no effect */
     let requestData: any = route.data.requestcondition;
     requestData.condition = Object.assign(requestData.condition, route.params);
-    if(route.url[0].path == "training-center") {
-          requestData.condition['user_id'] = this.userId;
-          requestData.condition['type'] = "admin";
+    if (route.url[0].path == "training-center") {
+      requestData.condition['user_id'] = this.userId;
+      requestData.condition['type'] = "admin";
     }
 
     // lesson plan 
-    if(route.url[0].path == "lesson-plan-material") {
+    if (route.url[0].path == "lesson-plan-material") {
       requestData.condition['lesson_id_object'] = route.params.lesson_id_object;
       delete requestData.condition.associated_training;
       delete requestData.condition._id
@@ -56,25 +64,25 @@ public user_details:any;
     }
 
 
-    if(route.url[0].path == "training-center-dna") {
-      requestData.condition['associated_training']=route.params.associated_training;
-       requestData['user_id'] = this.userId;
-       requestData['type'] = this.userType;
-    //    requestData['type'] = "mentor";
-       requestData['associated_training']=route.params.associated_training;
+    if (route.url[0].path == "training-center-dna") {
+      requestData.condition['associated_training'] = route.params.associated_training;
+      requestData['user_id'] = this.userId;
+      requestData['type'] = this.userType;
+      //    requestData['type'] = "mentor";
+      requestData['associated_training'] = route.params.associated_training;
 
-}
-    if(route.url[0].path == "training-report") {
-      if(this.userType == "admin"){
+    }
+    if (route.url[0].path == "training-report") {
+      if (this.userType == "admin") {
 
       }
-      if(this.userType == "salesrep"){
+      if (this.userType == "salesrep") {
         requestData.condition['user_id'] = this.userId;
       }
-      if(this.userType == "user"){
+      if (this.userType == "user") {
         requestData.condition['user_id'] = this.userId;
       }
-}
+    }
     return new Promise((resolve) => {
       this._apiService.CustomRequest(route.data.requestcondition, route.data.endpoint).subscribe(api_object => {
         if (api_object) {
